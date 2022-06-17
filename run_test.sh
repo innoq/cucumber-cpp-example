e00df8ece9dfa4b73c5a232aba938541f22bb410
David Kamphausen <david.kamphausen@innoq.com>
2022-06-17 07:45:58 +0200

    Wait for steps-server to startup

--- run_test.sh
+++ run_test.sh
@@ -2,7 +2,16 @@
 cmake -Bbuild . -DENABLE_TESTS=ON
 make -Cbuild
 if test -f ./build/FeatureSteps ; then
+
+  echo "Starting steps"
   ./build/FeatureSteps 2>/dev/null &
+  echo "waiting until port is open"
+
+  while ! netstat -tna | grep 'LISTEN\>' | grep -q ':3902\>'; do
+    sleep 1 # time in seconds, tune it as needed
+  done
+
+
   cucumber
 fi
 
